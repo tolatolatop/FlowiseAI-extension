fc6101cbc25a7749bc32f2c3ad7a7b1eff9d4470
tolatolatop <tolatolatop@gmail.com>
2024-03-24 19:07:00 +0000

    细化配置文件

--- main.js
+++ main.js
@@ -3,7 +3,4 @@
 import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed@1.2.1/dist/web.js"
 var scriptTag = document.getElementById('chat_config');
 var chatConfig = JSON.parse(scriptTag.textContent);
-Chatbot.init({
-    chatflowid: chatConfig.chatflowid,
-    apiHost: chatConfig.apiHost
-})
+Chatbot.init(chatConfig)
