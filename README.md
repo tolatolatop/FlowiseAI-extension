04fb593bab5d424c36ade2231add4a4b03692bef
tolatolatop <tolatolatop@gmail.com>
2024-03-24 16:52:16 +0000

    add flowise module to page

---
+++ README.md
@@ -0,0 +1,2 @@
+## Source
+[web.1.2.1.js](https://cdn.jsdelivr.net/npm/flowise-embed@1.2.1/dist/web.js)
---
+++ main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed@1.2.1/dist/web.js"
+Chatbot.init({
+    chatflowid: "162d294c-29c3-4004-94a7-c3a64cfe3e0c",
+    apiHost: "http://localhost:3000",
+})
